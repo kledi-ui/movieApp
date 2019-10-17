04e9b4c553457e3e64a643aee75760d4a513cc65
hawkeye-cool <kledjon007@hotmail.com>
2019-10-17 19:44:20 +0200

    fixed glitches 2

--- src/components/Person.js
+++ src/components/Person.js
@@ -3,9 +3,12 @@ import React from 'react'
 function Person({cast}) {
   if(cast.profile_path){
     return (
-<div className="swiper-slide" style={{textAlign:"center"}} >
-      <img className="poster" src={`https://image.tmdb.org/t/p/original${cast.profile_path}`} alt=""/>
+<div className="swiper-slide" >
+      <div className="poster-div">
+      <img className="poster"src={`https://image.tmdb.org/t/p/original${cast.profile_path}`} alt=""/>
       <h4 className="poster-name">{cast.name}</h4>
+      </div>
+      
     </div>
     )
   }
